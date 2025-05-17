d2e15452aa77d595fcdb1db15a0efdf09161a9bf
Urubu-Card <eduardolegalsans@gmail.com>
2025-05-17 13:05:39 -0300

    Update instumentacao_login.py

--- instumentacao_login.py
+++ instumentacao_login.py
@@ -24,8 +24,6 @@ bl.validar_email(email)
 
 
 
-img_url = "https://media.discordapp.net/attachments/1310238818603106345/1373329491794989207/image_1.png?ex=682a0442&is=6828b2c2&hm=6bae7e56c55bd6a68910d441f6e27e8d0b495cde07d9f9a9c0e4d2aed1cf1a0f&=&format=webp&quality=lossless"
-
 st.markdown(f"""
     <style>
         .bg-img {{
@@ -33,11 +31,11 @@ st.markdown(f"""
             bottom: 10px;
             right: 10px;
             width: 150px;
-            opacity: 0.8;
-            z-index: -1;
+            opacity: 0.5;
+            z-index: 1;
         }}
     </style>
-    <img class="bg-img" src="{img_url}"/>
+    <img class="bg-img" src="https://cdn.discordapp.com/attachments/1310238818603106345/1373329491794989207/image_1.png?ex=682a0442&is=6828b2c2&hm=6bae7e56c55bd6a68910d441f6e27e8d0b495cde07d9f9a9c0e4d2aed1cf1a0f&"/>
 """, unsafe_allow_html=True)
 
 
