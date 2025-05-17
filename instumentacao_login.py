23305a8d1a30609904f0b25f6b6860858d5da3fd
Urubu-Card <eduardolegalsans@gmail.com>
2025-05-17 13:35:15 -0300

    Update instumentacao_login.py

--- instumentacao_login.py
+++ instumentacao_login.py
@@ -28,14 +28,16 @@ st.markdown(
     <style>
         .logo-img {{
             position: fixed;
-            top: 30px;
-            left: 15px;
-            width: 500px; /* aumente esse valor se quiser maior */
-            opacity: 1;
-            z-index: 1;
+            top: 0px;
+            left: 0px;
+            width: 400px; /* ajuste conforme necessário */
+            margin: 0;
+            padding: 0;
+            z-index: 0;
+            opacity: 0.3;  /* ajuste a transparência se quiser mais suave */
         }}
     </style>
-    <img class="logo-img" src="https://cdn.discordapp.com/attachments/1310238818603106345/1373329491794989207/image_1.png?ex=682a0442&is=6828b2c2&hm=6bae7e56c55bd6a68910d441f6e27e8d0b495cde07d9f9a9c0e4d2aed1cf1a0f&"/>
+    <img class="logo-img" src="https://raw.githubusercontent.com/Urubu-Card/instrumentacao/main/instrumentacao/instrumentacao_projeto/imagens/decorativa.png"/>
     """,
     unsafe_allow_html=True
 )
